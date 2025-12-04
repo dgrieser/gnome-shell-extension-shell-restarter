7c9dab9a116edeaafdb4bb489a4930fbdadd79b3
David Grieser <d.grieser@mittwald.de>
2025-12-04 15:17:10 +0100

    fix(prefs): update ExtensionPreferences import path
    - Change import to the new GNOME Shell extensions preferences module path
    - Align with updated API location to prevent load errors
    - Ensure the preferences window works with current GNOME version

--- shell-restarter@koolskateguy89.github.io/prefs.js
+++ shell-restarter@koolskateguy89.github.io/prefs.js
@@ -1,7 +1,7 @@
 import Adw from 'gi://Adw';
 import Gtk from 'gi://Gtk';
 import Gio from 'gi://Gio';
-import {ExtensionPreferences} from 'resource:///org/gnome/shell/extensions/extension.js';
+import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 export default class ShellRestarterPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
